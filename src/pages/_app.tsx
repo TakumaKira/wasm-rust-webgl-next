541f26c4b4ddcc1f999bcddad2565e37077a1e88
Takuma Kira <takuma@kirakiraworx.com>
2023-12-23 18:10:37 +0900

    [dev] Make wasm work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import * as wasm from 'wasm'
 
 export default function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    wasm.greet()
+  })
+
   return <Component {...pageProps} />
 }
