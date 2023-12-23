914e274cd0c350d09d1873003f3ae39d2be190e9
Takuma Kira <takuma@kirakiraworx.com>
2023-12-24 02:27:43 +0900

    [refactor] Remove unused code

--- wasm-cpp/hello.cpp
+++ wasm-cpp/hello.cpp
@@ -9,18 +9,5 @@ extern "C" {
   }
 }
 
-extern "C" {
-  void process_data(double* input, double* output, int size) {
-      int i;
-
-      assert(size > 0 && "size must be positive");
-      assert(input && output && "must be valid pointers");
-
-      for (i = 0; i < size; i++) {
-          output[i] = input[i] * input[i];
-      }
-  }
-}
-
 // Build WASM with the command below:
-// em++ hello.cpp -o hello.js -sMODULARIZE -sSINGLE_FILE=1 -sEXPORTED_FUNCTIONS=_hello_react,_process_data,_malloc,_free,getValue -sEXPORTED_RUNTIME_METHODS=ccall
+// em++ hello.cpp -o hello.js -sMODULARIZE -sSINGLE_FILE=1 -sEXPORTED_FUNCTIONS=_hello_react,_malloc,_free,getValue -sEXPORTED_RUNTIME_METHODS=ccall
